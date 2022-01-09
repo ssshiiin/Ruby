68df1b6b331fc29280287adc1fc5b7f8224a02b4
Takumi SHINGAI <s1f101902433@iniad.org>
2022-01-09 14:54:12 +0900

    クオーテーションのシングルとダブルの違い2

--- practice1/puts.rb
+++ practice1/puts.rb
@@ -1,8 +1,18 @@
 # リテラル特殊文字はダブルでしか使えない
 
 puts "Hello\nworld"
-# Hello
-# world
+### Hello
+### world
 
 puts 'Hello\nworld'
-# Hello\nworld
+### Hello\nworl
+
+### 文字列の中での式展開
+
+name = "ssshiiin"
+
+puts 'Hello #{name}'
+### Hello #{name}
+
+puts "Hello #{name}"
+### Hello ssshiiin
